872e941ec120ea1890ee9cb7ec2e90a132dfbdb8
magicGAT <magicgat@msn.com>
2020-01-30 17:49:59 -0800

    updates

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+	return render(request, 'accounts/login.html')
+
+def sign_up(request):
+	return render(request, 'accounts/signup.html')
+
+def logout(request):
+	return render(request, 'accounts/logout.html')
+
